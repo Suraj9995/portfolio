8051abea222e365ed0b95e9cdf6badb5891f5604
Suraj9995 <djsurajkuchekar@gmail.com>
2022-06-14 19:30:42 +0530

    update github pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello Sk</h1>
-    </div>
-  );
+class App extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello Sk</h1>
+                <p>Web-App Dev</p>
+            </div>
+        )
+    }
 }
 
 export default App;
